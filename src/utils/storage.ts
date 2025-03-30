b5f4df6dac3eb1cad007f4c4313f774c3d795a27
khafizovas <justhomosapiens121@gmail.com>
2025-03-30 21:22:39 +0300

    Исправила импорт

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,4 @@
-import type { Task } from '../store/todoStore';
+import type { Task } from '../store/taskStore';
 
 export const loadTasksFromLocalStorage = (): Task[] => {
   return JSON.parse(localStorage.getItem('tasks') || '[]');
